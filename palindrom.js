721491cc57d6e97558dacf491c027dc134ad8a6b
Ivan Ivanov <123873945+misteradvert@users.noreply.github.com>
2023-06-26 09:36:51 +0300

    Created new files with JS palindrom finction

---
+++ palindrom.js
@@ -0,0 +1,11 @@
+function palindrom (str) {
+    //нижний регистр
+str = str.toLowerCase();
+//переводим строку в массив
+let str2 = str.split('');
+
+return str2;
+
+}
+
+console.log(palindrom('hello'));
